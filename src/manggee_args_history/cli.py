0618675941abb41493ad81ee64bcb8ae139354de
mangG907 <mang.gee@gmail.com>
2024-07-25 14:43:58 +0900

    error message

--- src/manggee_args_history/cli.py
+++ src/manggee_args_history/cli.py
@@ -21,9 +21,17 @@ def cmd():
 
     if args.scount:
         print(f"-s => {args.scount}")
+        # TODO 명령어 카운트
     elif args.top:
         print(f"-t => {args.top}")
         if args.dt:
             print(f"-d => {args.dt}")
+            # TODO 특정 날짜의 명령어 TOP N
         else:
-            print("TODO")
+            print("TODO- 에러나 안내 메시지를 주면")
+            #parser.print_help()
+            parser.error("-t 옵션은 -d 옵션과 함께 사용하시오!")
+    else:
+        # TODO - 사용법을 출력한다
+        parser.print_help()
+
